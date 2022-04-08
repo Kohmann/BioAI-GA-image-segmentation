a7be2fbf914408f2890504f4355285978956e817
eivskeiv <eivinkoh@stud.ntnu.no>
2022-04-08 15:52:59 +0200

    Starting creating the Individual class

--- src/main/kotlin/Individual.kt
+++ src/main/kotlin/Individual.kt
@@ -3,7 +3,16 @@
  * Class representing an individual in the population.
  * Contains a chromosome and functions for calculating fitness, crossover and mutation.
  */
-class Individual() {
+enum class Direction {
+    LEFT, RIGHT, UP, DOWN, NONE
+}
 
+class Individual(private val geneSize: Int = 10) {
+    private val chromosome: Array<Direction> = construction()
+
+    private fun construction(): Array<Direction> {
+        // TODO: Implement heuristic construction of the chromosome. Now it's just random.
+        return Array(geneSize) { Direction.values().random() }
+    }
 
 }
